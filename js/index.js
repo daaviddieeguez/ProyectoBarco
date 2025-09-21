035f77a9c10f2e9fa39585918b9a9ddb3cc1078a
daaviddieeguez <daaviddieeguez@gmail.com>
2025-09-21 18:51:08 +0200

    feat: primera versión del juego de hundir la flota con tablero dinámico

--- js/index.js
+++ js/index.js
@@ -19,6 +19,8 @@ console.log("Columnas "+columna);
 
 let barcox = Math.floor(Math.random() * columna);
 let barcoy = Math.floor(Math.random() * fila);
+let humox = new Array(3);
+let humoy = new Array(3);
 
 console.log("El barco esta en: "+barcox+", "+barcoy);
 
@@ -26,11 +28,53 @@ let singanador = true;
 while(singanador){
     let x = prompt("Introduce la columna: (0-"+columna+")");
     let y = prompt("Introduce la fila (0-"+fila+")");
+    let i = 0;
+    let eshumo = false;
+
+
+    for(i = 0; i < humox.length && !eshumo; i++){
+        if(x == humox[i] && y == humoy[i]){
+            eshumo = true;
+        }
+    }
+    
     if(x == barcox && y == barcoy){
         alert("Has ganado");
-    }else if(x == humox && y == humoy){
-        alert("Humo");
+        singanador = false;
+    }else if(eshumo){
+        alert("Humo. El barco paso hace "+i+" casillas");
     }else{
         alert("Agua");
     }
+
+    humox.unshift(barcox);
+    humoy.unshift(barcoy);
+
+    if(humox.length>3){
+        humox.pop();
+        humoy.pop();
+    }
+
+    moverse();
+}
+
+function moverse(){
+    let posicion = Math.floor(Math.random() * 2);
+    console.log("La posicion es: "+posicion);
+    if(posicion == 1){
+        let x = Math.floor(Math.random() * 2);
+        if(barcox != 0 && x == 0){
+            barcox--;
+        }else{
+            barcox++;
+        }
+    }else{
+        let y = Math.floor(Math.random() * 2);
+        if(barcoy != 0 && y == 0){
+            barcoy--;
+        }else{
+            barcoy++;
+        }
+    }
+    console.log("El barco esta en: "+barcox+", "+barcoy);
 }
